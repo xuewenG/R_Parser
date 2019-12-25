808343cbb6472f357b93a7cea5376f6cac15b94a
xuewenG <gexuewen@outlook.com>
2019-12-25 21:52:28 +0800

    修改代码

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, END, WORD, Text
+from tkinter import Tk, Button, Label, END, WORD
 from tkinter.scrolledtext import ScrolledText
 
 import util.Printer as Printer
@@ -7,25 +7,35 @@ from interpreter.Process import process_all
 
 class UserInterface:
     def __init__(self):
-        self.main_window = Tk(className="R编译器")
-
-        # 开始解释执行按钮
-        self.button = Button(self.main_window, text="run", bg="#76b3e4",
-                             font=50, width=16, height=5, command=self.compile)
-        self.button.alpha = 0.5
-        self.button.pack()
+        self.main_window = Tk(className="r_parser")
+        # 设置主窗口标题
+        self.main_window.title("R语言解释器")
+        # 设置主窗口大小和居中
+        ws = self.main_window.winfo_screenwidth()
+        hs = self.main_window.winfo_screenheight()
+        w = 500
+        h = 800
+        # 计算 x, y 位置
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        self.main_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
         # 提示信息
         self.code_input_label = Label(self.main_window, text="代码输入框")
         self.code_input_label.pack()
         # 代码输入框
-        self.code_input = ScrolledText(self.main_window, height=15, wrap=WORD)
+        self.code_input = ScrolledText(self.main_window, height=35, wrap=WORD)
         self.code_input.pack()
         # 提示信息
-        result_output_label = Label(self.main_window, text="文本结果输出框")
+        result_output_label = Label(self.main_window, text="运行结果")
         result_output_label.pack()
         # 结果输出框
-        self.result_output = Text(self.main_window, height=30, width=80)
+        self.result_output = ScrolledText(self.main_window, height=18, width=80)
         self.result_output.pack()
+        # 开始解释执行按钮
+        self.button = Button(self.main_window, text="run", bg='#CCCCCC',
+                             font=50, width=9, height=2, command=self.compile)
+        self.button.alpha = 0.5
+        self.button.pack()
         # 进入消息循环体
         self.main_window.mainloop()
 
